05cd7fb9a606db4ad3cf9435a97900f9f165de83
Juan Manuel Menendez Hernandez <juanmamenendez15@gmail.com>
2022-02-11 19:42:16 +0000

    refactor(component): refactor UserRepository to improve performance
    This DataBase update will allow having more concurrent users connected at the same time. Also will
    be good for the admins that are mantaining the site. WARNING: Be sure to have the proper
    dependencies updated.

--- src/modules/User/repositories/IUserRepository.ts
+++ src/modules/User/repositories/IUserRepository.ts
@@ -1,5 +1,6 @@
 import { IUserCreateDto } from '../dtos/IUserCreateDto';
 
+// New repository change
 export interface IUserRepository {
   createUser(data: IUserCreateDto): Promise<void>;
   getUser(): Promise<IUserCreateDto[]>;
