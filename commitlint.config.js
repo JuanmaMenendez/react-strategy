092283e36ffc29f1d795233a2fb4fe8aadb4e1f7
Juan Manuel Menendez Hernandez <juanmamenendez15@gmail.com>
2022-02-13 21:16:23 +0000

    fix: fix "npm run release" compatibility "long body messag
    e with commitlint"

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,20 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+const Configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ['@commitlint/config-conventional'],
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    'body-max-line-length': [0, 'always', 100],
+  },
+  /*
+   * Custom URL to show upon failure
+   */
+  helpUrl:
+    'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+};
+
+module.exports = Configuration;
